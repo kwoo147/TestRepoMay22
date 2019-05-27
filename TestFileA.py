634f847176835a9ee099c288603aeaed74855a17
IsaHajiyev <50885804+IsaHajiyev@users.noreply.github.com>
2019-05-27 11:16:18 -0400

    Update TestFileA.py
    Added a few variables to the program.

--- TestFileA.py
+++ TestFileA.py
@@ -3,5 +3,9 @@ NAME: Test File A
 DESCRIPTION: This is a test file.
 DATE: May 21, 2019
 PROGRAMMER: VLC ICS4U Computer Science Class - K. Woo
-
 '''
+#Added a few variables to the program
+pi = 3.14
+name = "Isa"
+check1 = True
+
