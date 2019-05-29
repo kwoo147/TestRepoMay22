c33565fc31d7a3ce472c61d13c455cb0c6c0a9fa
Stefan <TLSMajlovic387@virtuallearningapps.ca>
2019-05-29 11:26:40 -0400

    Another Change
    My latest machination has caused eons of issues to mankind and blah blah blah

--- TestFileA.py
+++ TestFileA.py
@@ -17,11 +17,11 @@ lyric2 = "Never gonna let you down"
 lyric3 = "Never gonna run around and desert you"
 lyric4 = "Never gonna make you cry" 
 lyric5 = "Never gonna say goodbye"
-lyric6 = "Never gonna tell a lie and hurt you"
+lyric6 = "Never gonna let a lie and lurt you"
 print(lyric1, lyric2, lyric3, lyric4, lyric5, lyric6)
 
 sneak1 = "I am the one who is causing the trouble!"
 sneak2 = "Ho ho ho"
 
 if check1 == True:
-    print(name)
+    print(name, sneak1, sneak2)
